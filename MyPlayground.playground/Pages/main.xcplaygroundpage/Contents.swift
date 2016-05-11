dfe03c2936c3db9094e27a0c60fcfa31b77b83c0
JimCampagno <jimcampagno@gmail.com>
2016-05-11 19:36:12 -0400

    add playground file for real this time

---
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+/*: Outline
+ 
+ 
+ # Variables and Stuff
+ 
+ ### Readings associated with this lab
+ 
+ * [Variables & Constants](https://github.com/learn-co-curriculum/swift-variables-readme)
+ * [Print](https://github.com/learn-co-curriculum/swift-print-readme)
+ * [String, Int data types](https://github.com/learn-co-curriculum/swift-dataTypes-readme)
+ 
+ In the following questions, where we use the term `variable`, we are not specifying whether or not it's a variable that is declared with `var` or a constant which is declared with `let`. We are using this general term, leaving it up to you to decipher whether or not you need to use `var` or `let` within the solution.
+ */
+/*: question1
+ ### 1. Create a variable which represents your bank account. (What you should be thinking.. What is a good name for this variable that makes it easily readable for myself now and for the future me OR other developers I might be working with? Should I declare it using let or var? Should it be of type Int or String?
+ */
+// write your code here
+
+
+
+
+/*: question2
+ ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
+ */
+// write your code here
+
+
+
+
+
+
+
+
+
+
+/*:
+ [Solution](solution)
+ */
+// ❤️
---
+++ MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Go Back](@next)
+// 1
+var myBankBalance = 0 // using type inference
+var myBankBalance2: Int = 100 // being explicit, using type annotation
+
+
+// 2
+let puppyName = "Bella" // using type inference
+let puppyName2: String = "Bella" // using type annotation
