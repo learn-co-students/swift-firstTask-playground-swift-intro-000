ed886eda8aa131334de94bb485ea114baafba5ff
Megan <Megan@MacBook-Pro-105.local>
2016-09-22 11:52:54 -0700

    Edited solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,14 +15,14 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
-var bankBalance = 100000000
+var bankBalance = 100000000   //Also can explicitly declare as int
 
 
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
 // write your code here
-let puppyName = "Bella"
+let puppyName = "Bella"       //Also can explicityly declare as String
 
 
 
@@ -62,10 +62,9 @@ print("I now have $\(bankBalance).")
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
-//Line of code that would give an error:
-//puppyName = "New Name"
-//However the variable puppyName was declared a constant and cannot be changed
+
+puppyName = "New Name"       //Error because puppyName is a constant variable and cannot be changed
+
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
