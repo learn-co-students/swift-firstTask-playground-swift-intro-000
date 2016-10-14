282b897e9ad46d1f71064deacecaaec195848db7
AMGC63 <gian_moran@hotmail.com>
2016-10-13 21:13:56 -0500

    Question have been answer with code

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,15 +14,14 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
-
+var myAccountBalance:Int = 1000
 
 
 
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
+let myPuppyName:String = "Bella"
 
 
 
@@ -30,7 +29,7 @@
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
+print(myPuppyName)
 
 
 
@@ -38,32 +37,30 @@
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
-// write your code here
-
+print("I just got a new puppy named \(myPuppyName) and she is awesome!")
 
 
 
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
-// write your code here
-
+print("I have $\(myAccountBalance) in my bank account.")
 
 
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
-// write your code here
+myAccountBalance = 1100
 
+print("I now have $\(myAccountBalance) in my bank account.")
 
 
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
-
+//myPuppyName = "Daisy" It gives me an error if I assign Daisy to myPuppy Name because I declared it in the beginning as a constant. So, "Bella" is stored into the variable call myPuppyName, to change the puppy name, I would need to declare myPuppyName as a variable string because this will let me store anything I want to store in myPuppyName as long as it's a string.
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
