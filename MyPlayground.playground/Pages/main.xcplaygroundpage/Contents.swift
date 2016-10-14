c2ba6423611ad4c6eb831c9515a57ca6c2e0fc9f
Tim Stahl <timstahl@TIMs-MacBook-Pro.local>
2016-10-14 13:27:32 -0600

    First Task

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,15 +14,17 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
-
+var currentAccountBalance: Double = 3899.93
+print (currentAccountBalance)
+var balance = "I have \(currentAccountBalance) in my bank account."
 
 
 
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
+let newPuppy = "Bella"
+
 
 
 
@@ -30,31 +32,29 @@
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
-
-
+print (newPuppy)
 
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
-// write your code here
-
+var printSentence = "I just got a new puppy named \(newPuppy) and she is awesome!"
+print(printSentence)
 
 
 
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
-// write your code here
-
+print(balance)
 
 
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
-// write your code here
+currentAccountBalance = 3999.93
+balance = "I now have \(currentAccountBalance) in my bank account."
 
 
 
@@ -62,8 +62,8 @@
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
-
+newPuppy = "Lucky"
+//Cannot change the newPuppy as it's a constant and not a variable
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
