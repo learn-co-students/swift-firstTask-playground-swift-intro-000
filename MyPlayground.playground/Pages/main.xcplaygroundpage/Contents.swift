b52a08e609a8511602ac08f8eacf3d581c93c2f5
twant <taylor.want@gmail.com>
2016-10-14 21:25:56 -0500

    solved?

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -17,13 +17,15 @@
 // write your code here
 
 
-
+var balance: Int
+balance=0
 
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
 // write your code here
 
+var pupName="Bella"
 
 
 
@@ -32,14 +34,14 @@
 */
 // write your code here
 
-
+print(pupName)
 
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
-
+print("I just got a new puppy named \(pupName) and she is awesome!")
 
 
 
@@ -49,21 +51,21 @@
 // write your code here
 
 
-
+print("I have \(balance) in my bank account")
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
 
-
-
+balance+=100
+print("I have \(balance) in my bank account")
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
-
+pupName="Ferd"
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
