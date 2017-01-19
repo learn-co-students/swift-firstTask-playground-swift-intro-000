0b3c18c4c6ea2168f5858eaa6e23a700b1161ae1
Morgan McCauley <morganmccauley@Morgans-MacBook-Pro.local>
2017-01-19 21:30:56 +1300

    first playground

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
-
+let s = "code overwritten when switching branch"
 
 
 
