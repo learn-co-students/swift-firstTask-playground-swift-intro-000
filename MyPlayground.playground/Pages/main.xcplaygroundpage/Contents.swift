2558e26db5cef8d268177c14ec98e9c4fcecdfee
iwpeifer <iwpeifer@gmail.com>
2017-02-24 14:58:43 -0700

    complete questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
-
+var bank_account_balance: Int = 100
 
 
 
@@ -24,7 +24,7 @@
 */
 // write your code here
 
-
+let puppy_name = "Bella"
 
 
 /*: question3
@@ -33,14 +33,14 @@
 // write your code here
 
 
-
+print(puppy_name)
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
 
-
+print("I just got a new puppy named \(puppy_name) and she is awesome!")
 
 
 /*: question5
@@ -49,7 +49,7 @@
 // write your code here
 
 
-
+print("I have $\(bank_account_balance) in my bank account.")
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
@@ -57,14 +57,14 @@
 // write your code here
 
 
-
+print("I now have $\(bank_account_balance + 100).")
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
 
-
+puppy_name = "Fluffkinz"
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  
