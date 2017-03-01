db32db0f43670c0746eb62a4ccd848de93f279e7
Ryan Lee <rlee33@gmail.com>
2017-03-01 13:35:24 -0600

    final

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,23 +14,20 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
-
-
+var bankAccountBalance: Int = 24957
 
 
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
-
+let puppyName: String = "Bella"
 
 
 
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
+print(puppyName)
 
 
 
@@ -38,15 +35,14 @@
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
-// write your code here
-
+print("I just got a new puppy named Bella and she is awesome!")
 
 
 
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
-// write your code here
+print("I have $\(bankAccountBalance) in my bank account.")
 
 
 
@@ -54,17 +50,17 @@
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
-// write your code here
-
+bankAccountBalance += 100
+print("I now have $\(bankAccountBalance).")
 
 
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
-
-
+puppyName = "Cocoa"
+// Cannot assign to vaule: 'puppyName' is a 'let' constant.
+// Constants are immutable and thus cannot be changed once set.
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  
