13aaefc329706177c209063c5c4cb8b7ade90086
ArtemisD <dianah456@gmail.com>
2017-03-27 18:53:06 -0700

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,9 +16,7 @@
 */
 // write your code here
 
-
-
-
+var balance = 100
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
@@ -26,13 +24,13 @@
 
 
 
-
+let puppyName = "Bella"
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
 // write your code here
 
-
+print(puppyName)
 
 
 /*: question4
@@ -41,21 +39,21 @@
 // write your code here
 
 
-
+print("I just got a new puppy named \(puppyName) and she is awesome!")
 
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
 // write your code here
 
-
-
+print("I have balance $\(balance) in my bank account.")
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
-
+balance = 200
+print("I now have $\(balance).")
 
 
 
@@ -63,7 +61,9 @@
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
+puppyName = "Something Else"
 
+//You get an error when you try to redeclare the value of a constant because you can't change the value of a constant.
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
