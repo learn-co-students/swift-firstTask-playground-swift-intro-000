4a388f7850642e325b37ae1c84372d58c781c7aa
Jonathan Boswell <jonathan@arborbridge.com>
2017-04-24 09:25:43 -0700

    Lab work

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,8 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
+var balance:Float = 0
+print(balance)
 
 
 
@@ -22,7 +23,7 @@
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
+let dog = "Bella"
 
 
 
@@ -30,15 +31,14 @@
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
-
+print(dog)
 
 
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
-// write your code here
+print("I just got a new puppy named \(dog) and she is awesome!")
 
 
 
@@ -46,7 +46,7 @@
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
-// write your code here
+print("I have $\(balance) in my bank account.")
 
 
 
@@ -54,7 +54,8 @@
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
-// write your code here
+balance += 100
+print("I now have $\(balance).")
 
 
 
@@ -62,7 +63,7 @@
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
+// No, you get an error
 
 
 /*:
