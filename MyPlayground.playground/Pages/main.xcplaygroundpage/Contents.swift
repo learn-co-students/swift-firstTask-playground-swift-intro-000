c9b5b6df0b2f51549ec40e7e3d221a24b71fe07d
ZellisE82 <zellis82@gmail.com>
2017-05-03 09:43:38 -0700

    Add answers

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,8 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
+var bankBalance: Int = 123123
+
 
 
 
@@ -22,15 +23,14 @@
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
-
+let newPuppy = " Bella"
 
 
 
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
+print(newPuppy)
 
 
 
@@ -40,21 +40,21 @@
 */
 // write your code here
 
+print("I just got a new puppy and her name is \(newPuppy)")
 
 
 
-/*: question5
-### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
-*/
-// write your code here
 
+print("I have a balance of \(bankBalance)")
 
 
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
-// write your code here
+bankBalance += 100
+
+print("I now have a balance of \(bankBalance)")
 
 
 
@@ -64,6 +64,7 @@
 */
 // write your code here
 
+// let is a constant and you cannot modify a constant once delcared.
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
