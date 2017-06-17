14dfa3f465d2d5174445ae899dd8c35237b9260d
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2017-06-17 16:42:13 -0400

    Completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
-
+var acctBal: Int = 2342
 
 
 
@@ -23,8 +23,7 @@
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
 // write your code here
-
-
+let pupName: String = "Donn"
 
 
 /*: question3
@@ -33,13 +32,14 @@
 // write your code here
 
 
+print(pupName)
 
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
-
+print("I just got a new puppy named \(pupName)")
 
 
 
@@ -48,7 +48,7 @@
 */
 // write your code here
 
-
+print("I have $\(acctBal) in my bank account")
 
 
 /*: question6
@@ -56,6 +56,8 @@
 */
 // write your code here
 
+acctBal = acctBal + 100
+print("I now have $\(acctBal)")
 
 
 
@@ -63,7 +65,7 @@
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
-
+pupName = "Cheese"
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
