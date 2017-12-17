4840e7d01943fec1906e9ec6ca65617780bae143
Scott C. Reynolds <scott@scottcreynolds.com>
2017-12-16 22:10:16 -0500

    do lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
-
+var accountBalance: Int
 
 
 
@@ -23,7 +23,7 @@
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
 // write your code here
-
+let puppyName = "Bella"
 
 
 
@@ -31,7 +31,7 @@
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
 // write your code here
-
+print(puppyName)
 
 
 
@@ -39,7 +39,7 @@
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
-
+print("I just got a new puppy named \(puppyName) and she is awesome!")
 
 
 
@@ -47,7 +47,8 @@
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
 // write your code here
-
+accountBalance = 300
+print("I have $\(accountBalance) in my bank account.")
 
 
 
@@ -55,7 +56,8 @@
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
-
+accountBalance += 100
+print("I now have $\(accountBalance).")
 
 
 
