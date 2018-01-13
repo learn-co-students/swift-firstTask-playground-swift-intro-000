3ea72d9c4d1f59c6789b0c6e1b68af8c5c2dba06
Ilias Aung <hello@iliasaung.com>
2018-01-13 17:34:40 -0500

    Added solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,7 +16,7 @@
 */
 // write your code here
 
-
+var balance: Int = 1500
 
 
 /*: question2
@@ -24,7 +24,7 @@
 */
 // write your code here
 
-
+let puppyName = "Bella"
 
 
 /*: question3
@@ -33,14 +33,14 @@
 // write your code here
 
 
-
+print(puppyName)
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
 
-
+print("I just got a new puppy named \(puppyName) and she is awesome!")
 
 
 /*: question5
@@ -48,7 +48,7 @@
 */
 // write your code here
 
-
+print("I have $\(balance) in my bank account")
 
 
 /*: question6
@@ -57,14 +57,15 @@
 // write your code here
 
 
-
+balance += 100
+print("I now have $\(balance)")
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
 
-
+puppyName = "Siri" //not possible to do so because it is a constant
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  
